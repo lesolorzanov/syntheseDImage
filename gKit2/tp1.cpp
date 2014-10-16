33e68fff8e6265bd369f66deb79318101eb52e77
Leslie Solorzano <zloy_smiertniy2@hotmail.com>
2014-10-16 16:20:58 +0200

    changement pour utiliser lancer rayon vraiment

--- gKit2/tp1.cpp
+++ gKit2/tp1.cpp
@@ -13,9 +13,11 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	float x0,y0,x1,y1,x2,y2,z0,z1,z2,z;
-    float r,cx,cy,cz;
+    float r,cx,cy,cz,angle;
 	z0=0.0f;z1=0.0f;z2=0.0f;z=0;
 	int width=512,height=512;
+    float znear=100.0;
+    float zfar=500.0;
     //montrer triangle, sphere ou sphere perturbe
 
     //string text = argv[1];
@@ -49,11 +51,36 @@ int main(int argc, char* argv[])
             cx = strtof(argv[3], NULL);
             cy = strtof(argv[4], NULL);
             cz = strtof(argv[5], NULL); 
+        }else if(argc == 9){
+            r = strtof(argv[2], NULL);
+            cx = strtof(argv[3], NULL);
+            cy = strtof(argv[4], NULL);
+            cz = strtof(argv[5], NULL); 
+            angle = strtof(argv[6], NULL); 
+            znear = strtof(argv[7], NULL); 
+            zfar = strtof(argv[8], NULL); 
+            cout<<"Radius "<<r<<"\n";
+            cout<<"centre sphere (repere propre)"<<cx<<","<<cy<<","<<cz<<"\n";
+            cout<<"Angle znear far "<<angle<<" ("<<(angle*M_PI/180.0)<<l.y<<","<<l.z<<"\n";
+            cout<<"OmC "<<OmC.x<<","<<OmC.y<<","<<OmC.z<<"\n";
+            cout<<"+-------------------------------------------------------------------+\n";
         }else if(argc == 2){
-            r  = 200.0f;
-            cx = 256.0f;
-            cy = 256.0f;
-            cz = 256.0f;
+            r  = 50.0f;
+            cx = 0.0f;
+            cy = 0.0f;
+            cz = -256.0f;
+        }
+    }else if(forme == 3) {//sphere reyes
+        if(argc == 6){
+            r = strtof(argv[2], NULL);
+            cx = strtof(argv[3], NULL);
+            cy = strtof(argv[4], NULL);
+            cz = strtof(argv[5], NULL); 
+        }else if(argc == 2){
+            r  = 50.0f;
+            cx = 0.0f;
+            cy = 0.0f;
+            cz = -256.0f;
         }
     }else{
         cout<<"Vous devez choisir un forme a afficher";
@@ -104,31 +131,127 @@ int main(int argc, char* argv[])
         	
         }   // chaque ligne
     }else if(forme==2.0f){
-        cout << "sphere\n";
-        float racine=0,norme=0; //constante dans ce cas
-        float dtotal=0;
+        //pour la version lancer de rayon, je peut utiliser gk::basicray, j'ai un origin et une direction
+        // avec ce rayon je trouve la distance du poin d'intersection avec l'sphere.
+        //http://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
 
+        //Je connais l'origin et le fin de mon rayon dans le repere viewport, mais ma sphere est au moins
+        //dans le repere view-camera j'asume que la matrice view camera et model sont l'identite.
+        //Apres, perspective et viewport je vais les faire. Je dois multiplier les points fin de mes
+        //rayons et comme ca j'aurai l'interesection des rayons avec le sphere. Apres je dois remetre
+        //ces intersections de retour au repere de l'image en multipliant par la matrice inverse.
+
+        gk::Transform viewport = gk::Viewport ( width,  height);
+        gk::Transform perspective = gk::Perspective (angle*M_PI/180.0, 1, znear, zfar);
+        gk::Transform T = viewport*perspective;
+        gk::Transform TInv = T.inverse();
+
+        cout << "sphere\n";
+        cout << "T\n";
+
+        T.matrix().print();
+        cout << "Tinv\n";
+        T.matrix().print();
+        cout << "\n";
+
+        //float racine=0,norme=0; //constante dans ce cas
+        //float dtotal=0;
+        float zdiff=zfar-znear;
         cout<<r<<" "<<cx<<" "<<cy<<" "<<cz<<" \n";
         for(int y= 0; y < width; y++) {
             for(int x= 0; x < height; x++){
+                //chaque pixel dans mon image est l'origine d'un rayon je dois transformer le rayon
+                //l'origin est a 0 le point final est a 1 (repere image) je transforme au repere view camera
+                float fx=(float)(x);
+                float fy=(float)(y);
+                gk::Point origRay = gk::Point(fx,fy,0.0);
+                gk::Point finRay = gk::Point(fx,fy,1.0);
+
+                origRay=TInv(origRay);
+                finRay=TInv(finRay);
+
+                //je dois trouver la direction du rayon...
+                gk::BasicRay rayon= gk::BasicRay(origRay,finRay);
+                gk::Vector l= rayon.d;
+                l/=l.Length();
+
+                //le centre sphere est dans le repere viewcamera deja
+                gk::Point cSphere = gk::Point(cx,cy,cz);
+
+
+                //dans ce point l'origin et le fin du rayon sont dans le repere view camera
+                //je dois trouver l'intersection du rayon avec le sphere et apres retourner a l'image pour dessiner..
+                gk::Vector OmC = origRay-cSphere;
+                float normeOmC=OmC.Length();
+                float loc=l.x*OmC.x + l.y*OmC.y + l.z*OmC.z;
+
+                float racine= loc*loc - normeOmC*normeOmC + r*r;
+
+                if(x%128==0 && y%128==0){
+
+                    cout<<"x, y "<<x<<","<<y<<"\n";
+                    cout<<"Origin Rayon "<<origRay.x<<","<<origRay.y<<","<<origRay.z<<"\n";
+                    cout<<"Fin Rayon "<<finRay.x<<","<<finRay.y<<","<<finRay.z<<"\n";
+                    cout<<"Direction Rayon "<<l.x<<","<<l.y<<","<<l.z<<"\n";
+                    cout<<"OmC "<<OmC.x<<","<<OmC.y<<","<<OmC.z<<"\n";
+                    cout<<"+-------------------------------------------------------------------+\n";
+
+                }
+
+                if(racine<0){
+                    image->setPixel(x, y, gk::VecColor(0,0,0)); 
+                }else{
+                    //transformer le point d'interesection ....
+                    //je viens de me rendre compte que je n'ai pas besoin de transformer le point XD
+                    float dist = sqrt(racine)-loc; 
+                    image->setPixel(x, y, gk::VecColor(dist/r,0.0,0.0));    // colorie chaque pixel en rouge
+                }  
+
                 //je profite de savoir que la direction du rayon est toujours Z
                 //norme o - c
+                /*float dx=(x-cx),dy=(y-cy),dz=(z-cz);
+                norme=dx*dx + dy*dy + dz*dz;
+                //on ne prends pas en compte dx et dy por la racine parce qu'ils sont multiplie par (0 0 1)
+                //en plus la norme a une racine carre mais dans la racine principal elle a un puissance 2 alors...
+                //on ne la met pas
+                racine=dz*dz-norme+r*r;
+
+                if(racine<0){
+                    image->setPixel(x, y, gk::VecColor(0,0,0)); 
+                }else{
+                    image->setPixel(x, y, gk::VecColor(sqrt(racine)/r,sqrt(racine)/r,sqrt(racine)/r));    // colorie chaque pixel en rouge
+                }     */     
+                image->setPixel(10, 10, gk::VecColor(1,1,1));           
+            }
+        }  
+    }
+    else if(forme==3.0f){
+        cout << "sphere reyes\n";
+        float racine=0,norme=0; //constante dans ce cas
+        //float dtotal=0;
+
+        cout<<r<<" "<<cx<<" "<<cy<<" "<<cz<<" \n";
+
+
+        for(int y= 0; y < width; y++) {
+            for(int x= 0; x < height; x++){
                 float dx=(x-cx),dy=(y-cy),dz=(z-cz);
                 norme=dx*dx + dy*dy + dz*dz;
-                /*on ne prends pas en compte dx et dy por la racine parce qu'ils sont multiplie par (0 0 1)
-                en plus la norme a une racine carre mais dans la racine principal elle a un puissance 2 alors...
-                on ne la met pas*/
-                racine=dz*dz-norme+r*r;
+                //jouer..
+                float zp=sqrt(r*r-x*x-y*y);
+
+                racine=dz*dz-norme+(r+zp)*(r+zp);
                 //cout<<norme<<" "<<racine<<" \n";
 
                 if(racine<0){
                     image->setPixel(x, y, gk::VecColor(0,0,0)); 
                 }else{
-                    float dp,dn;
+                    float dp;
+                    //float dn;
                     dp=-1*dz+sqrt(racine);
-                    dp=-1*dz-sqrt(racine);  
-                    dtotal=max(dp,dn);
-                    image->setPixel(x, y, gk::VecColor(dtotal/r,0, 0));    // colorie chaque pixel en rouge
+                    //dp=-1*dz-sqrt(racine);  
+                    //dtotal=max(dp,dn);
+                    image->setPixel(x, y, gk::VecColor(dp/r,0, 0));    // colorie chaque pixel en rouge
                 }                    
             }   
         }  
@@ -144,3 +267,4 @@ int main(int argc, char* argv[])
     delete image;
     return 0;
 }
+
